34650339332b377ff65c838ba8be46afc8008bc7
Re4lect <museflect@gmail.com>
2023-06-09 22:34:09 +0300

    done

--- Task1/src/Main.java
+++ Task1/src/Main.java
@@ -3,7 +3,12 @@ public class Main {
     // А-123
     // Б-321...
     // Коды даны для примера и не соответствуют действительности. Учитывайте, что нужно распечатать коды букв в двух регистрах!
+    public static final String CHARS_FOR_UNICODE_TEST = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя" +
+            "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
     public static void main(String[] args) {
         //todo код писать тут
+        for (int i = 0; i < CHARS_FOR_UNICODE_TEST.length(); i++) {
+            System.out.println(CHARS_FOR_UNICODE_TEST.charAt(i)+" == "+(int) CHARS_FOR_UNICODE_TEST.charAt(i));
+        }
     }
 }
--- Task2/src/Main.java
+++ Task2/src/Main.java
@@ -1,5 +1,40 @@
+import java.util.Scanner;
+
 public class Main {
+    public static final String CHARS_FOR_UNICODE_TEST = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя" +
+            "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ -";
+
     public static void main(String[] args) {
-        //todo код писать тут. По желанию(рекомендуется) вынести код в методы.
+        printFIO(testFullName(getFullName()));
     }
+
+    private static String getFullName() {
+        System.out.println("Введите ФИО через пробелы: ");
+        return new Scanner(System.in).nextLine();
+    }
+
+    private static String testFullName(String text) {
+        int spaceCounter = 0;
+        for (int i = 0; i < text.length(); i++) {
+            if (!CHARS_FOR_UNICODE_TEST.contains(String.valueOf(text.charAt(i)))) {
+                System.out.println("Введен неподдерживаемый символ!");
+                System.exit(1);
+            }
+            if (String.valueOf(text.charAt(i)).contains(" ")) {
+                spaceCounter++;
+            }
+
+        }
+        if (spaceCounter != 2){
+            System.out.println("Введено некорректное количество слов!");
+            System.exit(2);
+        }return text;
+    }
+    private static void printFIO(String fIO){
+        System.out.println("Фамилия: "+fIO.substring(0,fIO.indexOf(" ")));
+        System.out.println("Имя: "+fIO.substring(fIO.indexOf(" ")+1,fIO.lastIndexOf(" ")));
+        System.out.println("Отчество: "+fIO.substring(fIO.lastIndexOf(" ")+1));
+
+    }
+
 }
